c3d84339fbb37360862fa2e1224b2a05f70548e2
illiayatsentyuk <illiayatsentyuk@gmail.com>
2024-07-06 12:02:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+import mediaJSON from "./list/list_of_films.js";
+import express from "express";
+import {
+  logErrors,
+  clientErrorHandler,
+} from "./error_handling/error_hendler.js";
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+const app = express();
+
+app.get("/film", (req, res) => {
+  try {
+    if (req.query.id <= 0 || req.query.id > 13) {
+      res.send({
+        error: "Enter valid id!",
+      });
+    }
+    const film = mediaJSON[req.query.id - 1];
+    res.send(film);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("*", (req, res) => {
+  res.send({
+    error: "No page found",
+  });
+});
+
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
